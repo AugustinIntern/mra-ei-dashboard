792104483c47e095b56e17a316c33aa56bdeca13
AugustinIntern <augustin.mbx@gmail.com>
2026-05-04 12:16:05 +0400

    features added user authentication with clerk

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
       <div className="relative">
         <NavBar />
         <div className="absolute right-4 top-1/2 z-50 -translate-y-1/2 md:right-8">
-          <UserButton afterSignOutUrl="/sign-in" />
+          <UserButton />
         </div>
       </div>
       <main className="flex-1 flex flex-col">
